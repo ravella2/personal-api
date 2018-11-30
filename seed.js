bd5b19fff27e95899ded2bba44de7b2bbe00b25c
ravella2 <rachele.avella@gmail.com>
2018-11-29 16:30:54 -0800

    added images to seed.js

--- seed.js
+++ seed.js
@@ -6,35 +6,35 @@ const dodgers_list = [
         position: "Pitcher",
         jerseyNumber: 22,
         hometown: "Dallas, Tx",
-        image: "#"
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/477132.jpg"
     },
     {
         name: "Enrique Hernandez",
         position: "Utility Player",
         jerseyNumber: 14,
         hometown: "Toa Baja, Puerto Rico",
-        image: "#"
+        image: "http://mlb.mlb.com/mlb/images/players/head_shot/571771.jpg"
     },
     {
         name: "Matt Kemp",
         position: "Outfielder",
         jerseyNumber: 27,
         hometown: "Midwest City, OK",
-        image: "#"
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/461314.jpg"
     },
     {
         name: "Justin Turner",
         position: "Third Baseman",
         jerseyNumber: 10,
         hometown: "Long Beach, CA",
-        image: "#"
+        image: "https://i1.wp.com/scvnews.com/wp-content/uploads/2017/09/Justin-Turner.jpg?resize=300%2C451&ssl=1"
     },
     {
         name: "Cody Bellinger",
         position: "First Baseman",
         jerseyNumber: 35,
         hometown: "Scottsdale, AZ",
-        image: "#"
+        image: "http://mlb.mlb.com/mlb/images/players/head_shot/641355.jpg"
     }
 ];
 
